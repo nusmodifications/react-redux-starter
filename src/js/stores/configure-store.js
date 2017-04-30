dc60605fbf8fa729926f822f14e4cafd6c8ba238
Tay Yang Shun <tay.yang.shun@gmail.com>
2017-04-30 20:17:06 +0800

    Upgrade more developer dependencies

--- src/js/stores/configure-store.js
+++ src/js/stores/configure-store.js
@@ -8,13 +8,12 @@ export default function configureStore() {
   const middlewares = [thunk, requestsMiddleware];
   if (process.env.NODE_ENV === 'development') {
     /* eslint-disable */
-    const createLogger = require('redux-logger');
-    /* eslint-enable */
-    const logger = createLogger({
+    const logger = require('redux-logger').createLogger({
       level: 'info',
       collapsed: true,
       duration: true,
     });
+    /* eslint-enable */
     middlewares.push(logger);
   }
   return createStore(rootReducer, applyMiddleware(...middlewares));
